ad996f3f9a2a4165ed44cc0127c9ecd999df83c5
Tetsuro Kurita <tkurita@mac.com>
2010-12-08 01:49:07 +0000

    [LapTime] improved build settings for compatibility with Mac OS X 10.4.

--- LapTime.h
+++ LapTime.h
@@ -1,5 +1,9 @@
 #include <Carbon/Carbon.h>
 
+#if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_5
+typedef SInt32                          SRefCon;
+#endif
+
 OSErr startTimerHandler(const AppleEvent *ev, AppleEvent *reply, SRefCon refcon);
 OSErr lapTimeHandler(const AppleEvent *ev, AppleEvent *reply, SRefCon refcon);
 OSErr stopTimerHandler(const AppleEvent *ev, AppleEvent *reply, SRefCon refcon);
